56f810ef4216fb1c0b35f0f3706bc10ff5b368fd
Matheus Mendes <matheus_mendess@live.com>
2024-12-26 12:43:13 -0300

    fix eslint errors

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,15 +1,11 @@
-import { useTranslations } from "next-intl";
+import { useTranslations } from 'next-intl';
+import { twJoin } from 'tailwind-merge';
 
 function ArrowIcon() {
   return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
+        // eslint-disable-next-line
         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
         fill="currentColor"
       />
@@ -18,10 +14,14 @@ function ArrowIcon() {
 }
 
 export default function Footer() {
-  const t = useTranslations("components.ui.footer");
+  const t = useTranslations('components.ui.footer');
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <ul
+        className={twJoin(
+          'font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 dark:text-neutral-300 md:flex-row md:space-x-4 md:space-y-0'
+        )}
+      >
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -30,7 +30,7 @@ export default function Footer() {
             href="https://github.com/matheusmendescosta"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">{t("github")}</p>
+            <p className="ml-2 h-7">{t('github')}</p>
           </a>
         </li>
         <li>
@@ -41,12 +41,12 @@ export default function Footer() {
             href="https://www.linkedin.com/in/matheusmendescosta/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">{t("linkedin")}</p>
+            <p className="ml-2 h-7">{t('linkedin')}</p>
           </a>
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} {t("licensed")}
+        © {new Date().getFullYear()} {t('licensed')}
       </p>
     </footer>
   );
