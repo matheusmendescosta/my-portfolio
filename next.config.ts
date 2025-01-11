a8a87b1270fb6d80e46b3d71714b4a26596bb61a
Matheus Mendes <matheus_mendess@live.com>
2025-01-11 12:36:28 -0300

    fix: production setup

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone" as "standalone" | "export" | undefined,
+};
 
 export default withNextIntl(nextConfig);
