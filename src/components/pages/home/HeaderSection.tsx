9289d7a0ee8850d59bcfab0e3513fadc536d0908
matheus mendes <matheus_mendess@live.com>
2025-03-18 15:11:41 -0300

    wip detailed description

--- src/components/pages/home/HeaderSection.tsx
+++ src/components/pages/home/HeaderSection.tsx
@@ -14,25 +14,18 @@ function HeaderSection() {
         whileTap={{ scale: 0.8 }}
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 0.3,
-          scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 },
-        }}
+        transition={{ duration: 0.3, scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 } }}
       >
         <Image src="/images/profile.png" width={100} height={100} alt="image profile" className="rounded-full" priority={true} />
       </motion.div>
       <motion.div
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 0.3,
-          delay: 1,
-          scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 },
-        }}
+        transition={{ duration: 0.3, delay: 1, scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 } }}
       >
         <div className="ml-4 mt-4 text-left">
-          <p className="text-lg font-bold text-black dark:text-gray-200">{t('name')}</p>
-          <p className="text-md font-semibold text-black dark:text-gray-300">{t('occupation')}</p>
+          <p className="text-lg font-bold dark:text-gray-200">{t('name')}</p>
+          <p className="text-md font-semibold dark:text-gray-300">{t('occupation')}</p>
           <ContactSection />
         </div>
       </motion.div>
--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -9,11 +9,7 @@ import WorkExperienceSection from './WorkExperienceSection';
 function HomePage() {
   const { scrollYProgress } = useScroll();
 
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
+  const scaleX = useSpring(scrollYProgress, { stiffness: 100, damping: 30, restDelta: 0.001 });
 
   return (
     <>
@@ -22,60 +18,35 @@ function HomePage() {
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            delay: 1.25,
-            duration: 0.3,
-            scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 },
-          }}
+          transition={{ delay: 1.25, duration: 0.3, scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 } }}
         >
           <AboutSection />
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            delay: 1.5,
-            duration: 0.3,
-            scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 },
-          }}
+          transition={{ delay: 1.5, duration: 0.3, scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 } }}
         >
           <EducationSection />
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            delay: 1.5,
-            duration: 0.3,
-            scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 },
-          }}
+          transition={{ delay: 1.5, duration: 0.3, scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 } }}
         >
           <WorkExperienceSection />
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            delay: 2,
-            duration: 0.3,
-            scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 },
-          }}
+          transition={{ delay: 2, duration: 0.3, scale: { type: 'spring', visualDuration: 0.4, bounce: 0.4 } }}
         >
           <ProjectsSection />
         </motion.div>
       </div>
       <motion.div
         id="scroll-indicator"
-        style={{
-          scaleX,
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          height: 5,
-          originX: 0,
-          backgroundColor: '#A3AABE',
-        }}
+        style={{ scaleX, position: 'fixed', top: 0, left: 0, right: 0, height: 5, originX: 0, backgroundColor: '#A3AABE' }}
       />
     </>
   );
